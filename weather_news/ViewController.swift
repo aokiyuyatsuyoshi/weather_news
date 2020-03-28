a536778339b544e8171fedb3673552d3c71f7b0d
aokiyuyatsuyoshi <rainarrow0114@ezweb.ne.jp>
2020-03-28 17:21:20 +0900

    Initial Commit

---
+++ weather_news/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weather_news
+//
+//  Created by Yuya Aoki on 2020/03/28.
+//  Copyright © 2020 Yuya Aoki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
