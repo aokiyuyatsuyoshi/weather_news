ba368643deed2fa589270aa974213aacf2664774
aokiyuyatsuyoshi <rainarrow0114@ezweb.ne.jp>
2020-03-30 15:51:45 +0900

    first commit

---
+++ weather_news/prefectures.swift
@@ -0,0 +1,13 @@
+//
+//  prefectures.swift
+//  weather_news
+//
+//  Created by Yuya Aoki on 2020/03/29.
+//  Copyright © 2020 Yuya Aoki. All rights reserved.
+//
+
+import UIKit
+
+class prefectures: Object {
+
+}
